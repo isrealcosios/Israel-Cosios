4f335a67c74bde10e34f9f5c7012dc96aa487eb7
isrealcosios <74752909+isrealcosios@users.noreply.github.com>
2022-06-19 23:40:03 -0500

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,30 @@
+const btnSwitch = document.querySelector("#switch");
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+});
+const backToTopButton = document.querySelector("#back-to-top");
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 300) {
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnExit");
+            backToTopButton.classList.add("btnEntrance");
+            backToTopButton.style.display = "block";
+        }
+    } else {
+        if (!backToTopButton.classList.contains("btnEntrance")) {
+            backToTopButton.classList.remove("btnEntrance");
+            backToTopButton.classList.add("btnExit");
+            backToTopButton.style.display = "none";
+        }
+    }
+}
+
+backToTopButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    window.scrollTo(0, 0);
+}
